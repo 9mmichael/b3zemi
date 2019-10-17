36a4552d71a2e9e88f94ee688bd9af2d9db554dd
9mmichael <ueno.michael@gmail.com>
2019-10-17 14:53:44 +0900

    clear

--- b3zemi3/primePicker.py
+++ b3zemi3/primePicker.py
@@ -2,11 +2,9 @@ import math
 import time
 import pdb
 
-# def main():
 n = input('数字を入力してください: ')
 n = int(n)
-# if isinstance(n, int):
-#     raise TypeError('int型で入力してください')
+
 if n < 2:
     raise ValueError('2以上を入力してください')
 
@@ -27,20 +25,16 @@ for i in range(1, floor_sqrt_ni):
 
     # NOTE: 2 * i (i + 1) == p^2
     for j in range(2 * i * (i + 1), len(prime), p):
-    # for j in range(p * p, len(prime), p):
         prime[j] =  False
 
 
 time_end = time.time()
 tim = round(time_end - time_start, 8)
 print('{}秒'.format(tim))
-# print('hoge{}です'.format([i for i in range(n + 1) is prime[i]]))
+
 prime_count = prime.count(True)
 print('{0}未満の素数の個数は{1}です'.format(n, prime_count))
 
 for i in range(1, len(prime)):
     if prime[i] == True:
         print('{}'.format(i))
-# prime_file = open('prime.txt', 'w')
-# prime_file.writelines(data)
-# prime_file.close()
