95739d7b77a76b886088f5ca9ef1906d80ac0002
9mmichael <ueno.michael@gmail.com>
2019-10-17 14:53:01 +0900

    偶数除去

--- b3zemi3/primePicker.py
+++ b3zemi3/primePicker.py
@@ -1,5 +1,6 @@
 import math
 import time
+import pdb
 
 # def main():
 n = input('数字を入力してください: ')
@@ -11,22 +12,22 @@ if n < 2:
 
 time_start = time.time()
 
-
-prime = [True] * n
+half_n = math.ceil(n / 2)
+prime = [True] * half_n
 prime[0] = False
-prime[1] = False
 
 sqrt_n = math.sqrt(n)
-floor_n = math.floor(n)
+floor_sqrt_n = math.floor(sqrt_n)
+floor_sqrt_ni = math.ceil(floor_sqrt_n / 2)
 
-for i in range(2, floor_n):
-    if not  prime[i]:
+for i in range(1, floor_sqrt_ni):
+    if not prime[i]:
         continue
-    # p = 2 * i + 1
+    p = 2 * i + 1
 
     # NOTE: 2 * i (i + 1) == p^2
-    # for j in range(2 * i * (i + 1), len(prime), p):
-    for j in range(i * i, n, i):
+    for j in range(2 * i * (i + 1), len(prime), p):
+    # for j in range(p * p, len(prime), p):
         prime[j] =  False
 
 
@@ -35,7 +36,7 @@ tim = round(time_end - time_start, 8)
 print('{}秒'.format(tim))
 # print('hoge{}です'.format([i for i in range(n + 1) is prime[i]]))
 prime_count = prime.count(True)
-print('{0}以下の素数の個数は{1}です'.format(n, prime_count))
+print('{0}未満の素数の個数は{1}です'.format(n, prime_count))
 
 for i in range(1, len(prime)):
     if prime[i] == True:
