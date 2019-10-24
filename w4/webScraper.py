32ab033488feed85958fc5c818f1b89b77430f1b
9mmichael <ueno.michael@gmail.com>
2019-10-24 13:19:40 +0900

    3層目のスクレイピング対応

--- w4/webScraper.py
+++ w4/webScraper.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-# import os
 
 def scrape_web(url):
     for i in range(3):
@@ -12,10 +11,9 @@ def scrape_web(url):
             print()
             return
 
-        soup = BeautifulSoup(get_url_info.text, 'lxml')
+        soup = BeautifulSoup(get_url_info.content, 'lxml')
         url_list = []
 
-        # FIXME: titleタグあるのに文字化け(?)してしまうことがある
         try:
             print(soup.title.string)
         except:
@@ -30,7 +28,7 @@ def scrape_web(url):
             href_path = a_tag_list.get('href')
             if href_path is None:
                 continue
-            if href_path.startswith(('https://', 'http://')):
+            if href_path.startswith(('https://', 'http://')) and not href_path in url_list:
                 url_list.append(href_path)
                 print(href_path)
 
@@ -54,25 +52,12 @@ def double_loop_scrape(second_url_list):
 # NOTE: def main
 input_url = input('URL入力してな: ')
 
-# TODO: 何層スクレイピングするかの指定
-"""
-loop_count = input('何層スクレイピングする？: ')
-if not type(loop_count is int):
-    print('int型でよろしゅう')
-    sys.exit(status=None)
-loop_count = int(loop_count)
-if loop_count < 1:
-    print('1層以上でよろしゅう')
-    sys.exit(status=None)
-if loop_count > 5:
-    print('5層以上はきついで...')
-    sys.exit(status=None)
-"""
-
 print('---1---')
 input_url_list = scrape_web(input_url)
 print('---2---')
 second_url_list = loop_scrape(input_url_list)
 
-# print('---3---')
-# double_loop_scrape(second_url_list)
+third_url_show = input('3層目のURLリストアップする？ y/n: ')
+if third_url_show in {'y', 'yes'}:
+    print('---3---')
+    double_loop_scrape(second_url_list)
